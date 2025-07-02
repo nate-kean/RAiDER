d7a60951d61075c43d7dd7cf949f08b0b1d2f36f
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2025-07-01 19:20:41 -0500

    (debug) print whether any paths change

--- .circleci/fix_coverage_paths.py
+++ .circleci/fix_coverage_paths.py
@@ -2,16 +2,24 @@ import re
 import sqlite3
 import sys
 
+import numpy as np
+
 
 def main(file, replacement):
     conn = sqlite3.connect(file)
     cur = conn.cursor()
     cur.execute("select path, id from file")
 
+    results = list(cur)
     patched = [
         (re.sub(r"/.*/RAiDER/", replacement, r[0]), r[1])
         for r in cur
     ]
+
+    print(
+        "Has any file path been changed?",
+        np.any(np.array(results) != np.array(patched))
+    )
 
     cur.executemany("update file set path=? where id=?", patched)
 
