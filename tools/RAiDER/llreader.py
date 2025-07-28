65a126fff79502afbed3efd224ea3bbf64572b61
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2025-07-28 15:43:32 -0500

    Use multiline string for docstring

--- tools/RAiDER/llreader.py
+++ tools/RAiDER/llreader.py
@@ -1,11 +1,12 @@
-# noqa: D100
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-# Author: Jeremy Maurer, Raymond Hogenson & David Bekaert
-# Copyright 2019, by the California Institute of Technology. ALL RIGHTS
-# RESERVED. United States Government Sponsorship acknowledged.
-#
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ Author: Jeremy Maurer, Raymond Hogenson & David Bekaert
+ Copyright 2019, by the California Institute of Technology. ALL RIGHTS
+ RESERVED. United States Government Sponsorship acknowledged.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
 import os
 from pathlib import Path
 from typing import Optional, Union
