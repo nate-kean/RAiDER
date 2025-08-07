2ce88ff7b1400197babbbac4dfdbfaaee68bdc93
Nate Kean <natekean65535@gmail.com>
2025-08-07 18:59:08 -0500

    Format

--- tools/RAiDER/models/gmao.py
+++ tools/RAiDER/models/gmao.py
@@ -120,10 +120,30 @@ class GMAO(WeatherModel):
                 logger.warning('Weather model already exists, skipping download')
 
             with h5py.File(path, 'r') as ds:
-                q = ds['QV'][0, :, lat_min_ind : (lat_max_ind + 1), lon_min_ind : (lon_max_ind + 1)]
-                p = ds['PL'][0, :, lat_min_ind : (lat_max_ind + 1), lon_min_ind : (lon_max_ind + 1)]
-                t = ds['T'][0, :, lat_min_ind : (lat_max_ind + 1), lon_min_ind : (lon_max_ind + 1)]
-                h = ds['H'][0, :, lat_min_ind : (lat_max_ind + 1), lon_min_ind : (lon_max_ind + 1)]
+                q = ds['QV'][
+                    0,
+                    :,
+                    lat_min_ind : (lat_max_ind + 1),
+                    lon_min_ind : (lon_max_ind + 1),
+                ]
+                p = ds['PL'][
+                    0,
+                    :,
+                    lat_min_ind : (lat_max_ind + 1),
+                    lon_min_ind : (lon_max_ind + 1),
+                ]
+                t = ds['T'][
+                    0,
+                    :,
+                    lat_min_ind : (lat_max_ind + 1),
+                    lon_min_ind : (lon_max_ind + 1),
+                ]
+                h = ds['H'][
+                    0,
+                    :,
+                    lat_min_ind : (lat_max_ind + 1),
+                    lon_min_ind : (lon_max_ind + 1),
+                ]
             path.unlink()
 
         lats = np.arange(
