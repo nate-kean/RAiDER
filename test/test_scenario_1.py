f052fe20beb1e8985f632ebfa4f5fc1d99780594
Nate Kean <natekean65535@gmail.com>
2025-08-07 19:51:55 -0500

    Add parameter for GMAO times earlier than T0

--- test/test_scenario_1.py
+++ test/test_scenario_1.py
@@ -20,11 +20,21 @@ WM_LOC = SCENARIO_DIR / 'weather_files'
 
 _RTOL = 1e-2
 DATETIME = dt.datetime(2018, 7, 1, 0, 0)
+DATETIME_GMAO_OLD = dt.datetime(2017, 1, 1, 0, 0, 0)
 
 
 @pytest.mark.long
-@pytest.mark.parametrize('Model', (ERA5, ERA5T, GMAO, MERRA2))
-def test_tropo_delay(tmp_path: Path, Model: Type[WeatherModel]) -> None:
+@pytest.mark.parametrize(
+    'Model,time',
+    (
+        pytest.param(ERA5, DATETIME, id='ERA5'),
+        pytest.param(ERA5T, DATETIME, id='ERA5T'),
+        pytest.param(GMAO, DATETIME, id='GMAO new'),
+        pytest.param(GMAO, DATETIME_GMAO_OLD, id='GMAO old'),
+        pytest.param(MERRA2, DATETIME, id='MERRA2'),
+    ),
+)
+def test_tropo_delay(tmp_path: Path, Model: Type[WeatherModel], time: dt.datetime) -> None:
     """
     Scenario:
     1: Small area, Zenith delay.
@@ -39,10 +49,10 @@ def test_tropo_delay(tmp_path: Path, Model: Type[WeatherModel]) -> None:
 
     wm = Model()
     wm.set_latlon_bounds(aoi.bounds())
-    wm.setTime(DATETIME)
+    wm.setTime(time)
     wm.set_wmLoc(str(WM_LOC))
-    wm_file_path = prepareWeatherModel(wm, DATETIME, aoi.bounds())
-    wet, hydro = tropo_delay(DATETIME, wm_file_path, aoi, los, zref=20_000)
+    wm_file_path = prepareWeatherModel(wm, time, aoi.bounds())
+    wet, hydro = tropo_delay(time, wm_file_path, aoi, los, zref=20_000)
 
     # load the true delay
     true_wet, _ = rio_open(SCENARIO_DIR / f'{wm._Name}/wet.envi', userNDV=0.0)
