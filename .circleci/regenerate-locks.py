619a55adf506e52a62458ffbc7393221bdda23a7
Nate Kean <natekean65535@gmail.com>
2025-08-25 10:54:42 -0500

    Fix yaml typo: use fuzzy = instead of exact ==
    My bad, I did not know they were different: https://stackoverflow.com/questions/69734856/whats-the-difference-between-and-for-conda-package-versioning
    
    This was causing conda-lock to fail to solve due to unnecessarily restricing the Python version to 3.x.0 (e.g., 3.9.0 whereas current is 3.9.23).

--- .circleci/regenerate-locks.py
+++ .circleci/regenerate-locks.py
@@ -38,8 +38,8 @@ def generate_lock(out_path: Path, version: str, template: str) -> None:
         env_path = tmp_dir / 'environment.yml'
 
         # Hardcode a copy of the environment.yml file to this Python version
-        with env_path.open('w', encoding='utf-8') as f_env:
-            f_env.write(re.sub(PATTERN_PYTHON_DEP, f'\\1=={version}', template))
+        with env_path.open('w', encoding='utf-8') as f_tmp_env:
+            f_tmp_env.write(re.sub(PATTERN_PYTHON_DEP, f'\\1={version}', template))
 
         # Platforms explicitly listed in order to exclude win-64, since isce3
         # and wand (and therefore RAiDER) are not compatible with Windows.
